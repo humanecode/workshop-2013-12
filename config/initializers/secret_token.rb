ef712b1e7b8357974cc628109daa56cf301d6cd3
Mike Moore <mike@blowmage.com>
2013-12-10 16:51:17 -0700

    Empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DailyStandup::Application.config.secret_key_base = '812970f6b99f90017e2d5f7685d295fb31776e6acb7782ea0ba0e7fe473fcd7d0ccbeb9e6fe6c4916aafdb239ecffc635accc453664fd68778ee035547d2b2ba'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DailyStandup::Application.config.session_store :cookie_store, key: '_daily_standup_session'
