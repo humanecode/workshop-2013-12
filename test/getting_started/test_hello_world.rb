f1a12470aede1a65e82cc270b086472f53bc8034
Mike Moore <mike@blowmage.com>
2013-12-10 17:32:29 -0700

    Add greeting with name test
    $ ruby test/getting_started/test_hello_world.rb
    Run options: --seed 10669
    
    # Running tests:
    
    ..
    
    Finished tests in 0.000698s, 2865.3295 tests/s, 2865.3295 assertions/s.
    
    2 tests, 2 assertions, 0 failures, 0 errors, 0 skips

--- test/getting_started/test_hello_world.rb
+++ test/getting_started/test_hello_world.rb
@@ -10,4 +10,8 @@ class HelloWorldTest < MiniTest::Unit::TestCase
   def test_greeting
     assert_equal "Hello world!", HelloWorld.new.greet
   end
+
+  def test_greeting_with_name
+    assert_equal "Hello Mike!", HelloWorld.new.greet("Mike")
+  end
 end
