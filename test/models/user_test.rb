2ad91f06b85a140d69d4218245a23d5302844083
Mike Moore <mike@blowmage.com>
2013-12-10 17:56:25 -0700

    Remove create X record tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -27,31 +27,6 @@ class UserTest < ActiveSupport::TestCase
     refute_valid user
   end
 
-  def test_create_1_record
-    assert_difference "User.count", 1 do
-      create_random_user
-    end
-  end
-
-  def test_create_3_records
-    assert_difference "User.count", 3 do
-      3.times { create_random_user }
-    end
-  end
-
-  def test_create_10_records
-    assert_difference "User.count", 10 do
-      10.times { create_random_user }
-    end
-  end
-
-  def create_random_user
-    random = SecureRandom.hex(8)
-    User.create! email: "#{random}@example.com",
-                 password: random,
-                 password_confirmation: random
-  end
-
   def assert_valid user
     assert_predicate user, :valid?
   end
