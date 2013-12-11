4783698d843340522dc64f4e0a9225055e37f0b4
Mike Moore <mike@blowmage.com>
2013-12-10 17:53:18 -0700

    Add create X record tests
    $ rake test
    Run options: --seed 58392
    
    # Running tests:
    
    ...............
    
    Finished tests in 0.605224s, 24.7842 tests/s, 42.9593 assertions/s.
    
    15 tests, 26 assertions, 0 failures, 0 errors, 0 skips

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -27,6 +27,31 @@ class UserTest < ActiveSupport::TestCase
     refute_valid user
   end
 
+  def test_create_1_record
+    begin_count = User.count
+    create_random_user
+    assert_equal begin_count + 1, User.count
+  end
+
+  def test_create_3_records
+    begin_count = User.count
+    3.times { create_random_user }
+    assert_equal begin_count + 3, User.count
+  end
+
+  def test_create_10_records
+    begin_count = User.count
+    10.times { create_random_user }
+    assert_equal begin_count + 10, User.count
+  end
+
+  def create_random_user
+    random = SecureRandom.hex(8)
+    User.create! email: "#{random}@example.com",
+                 password: random,
+                 password_confirmation: random
+  end
+
   def assert_valid user
     assert_predicate user, :valid?
   end
