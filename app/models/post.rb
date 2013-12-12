1af6d61c47578f62ee1e5b2b26f47c0be76400ed
Mike Moore <mike@blowmage.com>
2013-12-12 07:02:42 -0600

    Add Post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ActiveRecord::Base
   belongs_to :group
   belongs_to :user
+
+  validates :group_id, presence: true
+  validates :user_id, presence: true
+  validates :date, presence: true
+  validates :user_id, uniqueness: { scope: [:group_id, :date] }
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,40 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_can_be_valid
+    post = posts :devops_mike_today
+    assert post.valid?
+  end
+
+  def test_validates_group_id
+    post = posts :devops_mike_today
+    post.group = nil
+    refute post.valid?
+    assert_includes post.errors[:group_id], "can't be blank"
+  end
+
+  def test_validates_user_id
+    post = posts :devops_mike_today
+    post.user = nil
+    refute post.valid?
+    assert_includes post.errors[:user_id], "can't be blank"
+  end
+
+  def test_validates_date
+    post = posts :devops_mike_today
+    post.date = nil
+    refute post.valid?
+    assert_includes post.errors[:date], "can't be blank"
+  end
+
+  def test_validates_uniqueness
+    post = posts :devops_mike_today
+    post2 = Post.new
+    post2.group = post.group
+    post2.user = post.user
+    post2.date = post.date
+
+    refute post2.valid?
+    assert_includes post2.errors[:user_id], "has already been taken"
+  end
 end
