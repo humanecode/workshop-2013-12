a69b228be9d99e4e258389d0b74ae237dac5d51e
Mike Moore <mike@blowmage.com>
2013-12-12 07:01:19 -0600

    Add Membership validations

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,4 +1,8 @@
 class Membership < ActiveRecord::Base
   belongs_to :group
   belongs_to :user
+
+  validates :group_id, presence: true
+  validates :user_id, presence: true
+  validates :user_id, uniqueness: { scope: :group_id }
 end
--- test/models/membership_test.rb
+++ test/models/membership_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class MembershipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_can_be_valid
+    mem = memberships :payroll_mike
+    assert mem.valid?
+  end
+
+  def test_validates_group_id
+    mem = memberships :payroll_mike
+    mem.group = nil
+    refute mem.valid?
+    assert_includes mem.errors[:group_id], "can't be blank"
+  end
+
+  def test_validates_user_id
+    mem = memberships :payroll_mike
+    mem.user = nil
+    refute mem.valid?
+    assert_includes mem.errors[:user_id], "can't be blank"
+  end
+
+  def test_validates_uniqueness
+    mem = memberships :payroll_mike
+    mem2 = Membership.new
+    mem2.group = mem.group
+    mem2.user = mem.user
+
+    refute mem2.valid?
+    assert_includes mem2.errors[:user_id], "has already been taken"
+  end
 end
