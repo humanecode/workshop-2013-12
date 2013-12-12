88b7dc2d1fb95849a44a10bfbc22cd558345babb
Mike Moore <mike@blowmage.com>
2013-12-12 06:59:52 -0600

    Add Group#name presence validation

--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class GroupTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_can_be_valid
+    group = groups :payroll
+    assert group.valid?
+  end
+
+  def test_validates_name
+    group = groups :payroll
+    group.name = nil
+    refute group.valid?
+    assert_includes group.errors[:name], "can't be blank"
+  end
 end
